052245cfe23a85dc8c7cb3cec916ce9489cfec7b
Clint Goodman <clintg@adobe.com>
2025-05-27 08:25:18 -0600

    feat: step 12

--- embeddings.ts
+++ embeddings.ts
@@ -7,17 +7,24 @@ const embeddings = new OllamaEmbeddings({
 
 const vectorStore = await MemoryVectorStore.fromDocuments(
   [
-    { pageContent: 'water', metadata: { source: 'http://water.com' } },
-    { pageContent: 'internet', metadata: { source: 'http://internet.com' } },
-    { pageContent: 'space', metadata: { source: 'http://space.com' } },
-    { pageContent: 'joy', metadata: { source: 'http://joy.com' } },
-    { pageContent: 'cookies', metadata: { source: 'http://cookies.com' } },
+    {
+      pageContent:
+        'When clean water is out of reach, families are left to depend on unsafe sources, often leading to preventable illness. Children miss school, parents struggle to care for their homes, and dreams are put on hold. But together, we can offer the gift of health. Through clean water projects and education, we help families turn on a tap to safety, dignity, and brighter days ahead.',
+      metadata: { source: 'https://100humanitarians.org/' },
+    },
+    {
+      pageContent:
+        'Without access to menstrual hygiene products and safe spaces, too many girls are forced to miss school—or leave entirely—putting their futures at risk. What should be a natural part of growing up becomes a barrier to education and opportunity. Together, we can change this. By providing essential supplies and education, we help girls stay in school, stay empowered, and boldly shape their own paths forward.',
+      metadata: { source: 'https://100humanitarians.org/' },
+    },
+    {
+      pageContent:
+        'For many families, the cost of school fees stands between their children and a brighter future. Without access to education, young dreams are quietly set aside, and the cycle of poverty continues. Together, we can change the story. By helping cover school fees, we empower children to step into classrooms with confidence—and into lives filled with possibility.',
+      metadata: { source: 'https://100humanitarians.org/' },
+    },
   ],
   embeddings
 );
 
 const retriever = vectorStore.asRetriever(1);
-
-console.log(await retriever.invoke('wet'));
-// console.log(await retriever.invoke('technology'));
-// console.log(await retriever.invoke('misery'));
+console.log(await retriever.invoke('education')); // Note that education is present in all documents
